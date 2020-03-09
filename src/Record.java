154c7073160658e89e56a8aa7d9aea4adb2ab1fe
Ben <zhuzhiben@Bens-MacBook-Pro-2.local>
2020-03-09 18:22:05 -0400

    Record class

--- src/Record.java
+++ src/Record.java
@@ -1,2 +1,16 @@
 public class Record {
+    //ID: 4-byte integer (Primary Key)
+    //ClientName: 16-byte long string
+    //Phone: 12-byte long string
+    int ID;
+    String ClientName;
+    String Phone;
+    String tableName;
+
+    public Record(){}
+
+    @Override
+    public String toString(){
+        return "(" + ID + "," + ClientName + "," + Phone + ")";
+    }
 }
