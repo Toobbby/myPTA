eed3dec308757525b85b67dce68a4dd8b3c70301
Fangzheng Guo <gfz2000@foxmail.com>
2020-03-24 19:07:07 -0400

    LSM logging, LSM initialization

--- LSM/LSMTree.java
+++ LSM/LSMTree.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class LSMTree {
@@ -22,8 +23,10 @@ public class LSMTree {
     ArrayList<Integer> level_size;
     ArrayList<Integer> last_chosen;
     LSMBuffer<String, MemTable> buffer;
+    String logPath;
     int number=0;
-    public LSMTree(String tableName, int maxSize, String dir, LSMBuffer<String, MemTable> b){
+    public LSMTree(String tableName, int maxSize, String dir, LSMBuffer<String, MemTable> b, String logPath){
+        this.logPath = logPath;
         buffer=b;
         this.tableName = tableName;
         memTable=new MemTable(randomFileName());
@@ -34,10 +37,11 @@ public class LSMTree {
         last_chosen=new ArrayList<>();
         level0=new ArrayList<>();
         SStables=new ArrayList<>();
+        tableInitialization(fileBaseDir);
     }
 
 
-    public void flushMemtable(){
+    public void flushMemtable() throws IOException {
         if (memTable.size()>=maxSize){
             //need flush to level0
             if (level0.size()==0){
@@ -60,7 +64,7 @@ public class LSMTree {
         }
     }
 
-    public void deleteRecord(int id){
+    public void deleteRecord(int id) throws IOException {
         Record r=null;
         try {
             r=readID(id);
@@ -75,9 +79,10 @@ public class LSMTree {
     }
 
 
-    public void write(Record t){
+    public void write(Record t) throws IOException {
         //if Memtable is not full add to Memtable
         memTable.write(t);
+        logWriter("Written: " + t.toString(), this.logPath);
         flushMemtable();
     }
 
@@ -89,39 +94,41 @@ public class LSMTree {
             for(SSTable s: level0){
                 if (id>=s.begin&&id<=s.end) {
                     //could be optimized
-                    MemTable file=loadOrGetMemtable(s);
+                    MemTable file=loadOrGetMemtable(s, 0);
                     res = file.read(id);
                     if (res != null) return res;
                 }
             }
             //from level1
-            for (TreeSet<SSTable> level: SStables){
+            for(int i = 0; i < SStables.size(); i++){
+                TreeSet<SSTable> level = SStables.get(i);
                 SSTable temp=new SSTable();
                 temp.begin=id;
                 SSTable file=level.floor(temp);
                 if (file==null||file.end<id) return null;
-                MemTable table=loadOrGetMemtable(file);
+                MemTable table=loadOrGetMemtable(file, i + 1);
                 res = table.read(id);
                 if (res != null) return res;
             }
         }
-        if (res.Phone.equals("delete"))return null;
+        if (res != null && res.Phone.equals("delete")) return null;
         return res;
     }
 
     
-    public MemTable loadOrGetMemtable(SSTable sstable){
+    public MemTable loadOrGetMemtable(SSTable sstable, int level) throws IOException {
         if (buffer.containsKey(sstable.id.toString())){
             return buffer.get(sstable.id.toString());
         }else {
             MemTable res=new MemTable(sstable.read());
             buffer.put(sstable.id.toString(),res);
+            logWriter("Swap in  " + "L-" + level +"K" + sstable.id.substring(0,2) + sstable.begin + sstable.id.substring(0,2) + sstable.end, this.logPath);
             return res;
         }
     }
     //naive implementation
     //other way is to build a index using area code
-    public ArrayList<Record> readAreaCode(String areaCode){
+    public ArrayList<Record> readAreaCode(String areaCode) throws IOException {
         ArrayList<Record> res=new ArrayList<>();
         Iterator<Record> i=memTable.iterate();
         Record tempRecord;
@@ -130,30 +137,34 @@ public class LSMTree {
             tempRecord=i.next();
             if (tempRecord.getPhone().startsWith(areaCode)){
                 res.add(tempRecord);
+                logWriter("MRead: " + tempRecord.toString(), this.logPath);
             }
         }
         //loop in level 0
         for(SSTable s: level0){
                 //could be optimized
-                tempMentable = loadOrGetMemtable(s);
+                tempMentable = loadOrGetMemtable(s, 0);
                 i=tempMentable.iterate();
                 while (i.hasNext()){
                     tempRecord=i.next();
                     if (tempRecord.getPhone().startsWith(areaCode)){
                         res.add(tempRecord);
+                        logWriter("MRead: " + tempRecord.toString(), this.logPath);
                     }
                 }
         }
         //from level1
-        for (TreeSet<SSTable> level: SStables){
+        for(int j = 0; j < SStables.size(); j++){
+            TreeSet<SSTable> level = SStables.get(j);
             for(SSTable s: level){
                 //could be optimized
-                tempMentable = loadOrGetMemtable(s);
+                tempMentable = loadOrGetMemtable(s, j + 1);
                 i=tempMentable.iterate();
                 while (i.hasNext()){
                     tempRecord=i.next();
                     if (tempRecord.getPhone().startsWith(areaCode)){
                         res.add(tempRecord);
+                        logWriter("MRead: " + tempRecord.toString(), this.logPath);
                     }
                 }
             }
@@ -166,7 +177,7 @@ public class LSMTree {
     }
 
 
-    public void compaction(){
+    public void compaction() throws IOException {
         //compaction will always start from level0 and propagate to deeper level;
         //level0 compaction is treated specially
         compactLevel0();
@@ -179,7 +190,7 @@ public class LSMTree {
     }
 
     //compact level and write to level+1
-    public void compactLevel(int level){
+    public void compactLevel(int level) throws IOException {
         //pick one SStable from level
         SSTable temp=new SSTable();
         temp.begin=last_chosen.get(level-1)+1;
@@ -249,7 +260,7 @@ public class LSMTree {
 
 
 
-    public void compactLevel0(){
+    public void compactLevel0() throws IOException {
         ArrayList<SSTable> toCompact=new ArrayList<>();
         SSTable chosen=level0.get(3);
         toCompact.add(chosen);level0.remove(3);
@@ -258,7 +269,8 @@ public class LSMTree {
             SSTable s=level0.get(i);
             if (chosen.overlap(s)){
                 buffer.remove(s.id.toString());
-                toCompact.add(s);level0.remove(s);
+                toCompact.add(s);
+                level0.remove(s);
             }
         }
 
@@ -307,12 +319,15 @@ public class LSMTree {
     }
 
 
-    public void mergeAndWrite(ArrayList<SSTable> filesToMerge,int level) {
+    public void mergeAndWrite(ArrayList<SSTable> filesToMerge,int level) throws IOException {
         //filesToMerge need to add to list by time order. ie index 0 is newest value
         HashMap<Integer,SSTable> mergedTuples=new HashMap<>();
         for (int i=filesToMerge.size()-1;i>=0;--i){
             SSTable ss=filesToMerge.get(i);
-            MemTable temp=loadOrGetMemtable(ss);
+            String[] getssLevel = ss.loc.split("/");
+            String sslevel = getssLevel[getssLevel.length - 2];
+            String number = sslevel.substring(5);
+            MemTable temp=loadOrGetMemtable(ss, Integer.parseInt(number));
             for (Integer integer : temp.tuples.keySet()) {
                 mergedTuples.put(integer,ss);
             }
@@ -320,20 +335,30 @@ public class LSMTree {
         ArrayList<Integer> keys=new ArrayList<Integer>(mergedTuples.keySet());
         Collections.sort(keys);
         ArrayList<Record> buffer=new ArrayList<>();
+        SSTable start=new SSTable(),end=new SSTable();
+        start.begin=keys.get(0);
         for (Integer k:keys){
+            if (start.begin==-1){
+                start.begin=k;
+            }
+            end.begin=k;
             SSTable recordLoc=mergedTuples.get(k);
-            MemTable source=loadOrGetMemtable(recordLoc);
+            String[] getLevel = recordLoc.loc.split("/");
+            String mylevel = getLevel[getLevel.length - 2];
+            String number = mylevel.substring(5);
+            MemTable source=loadOrGetMemtable(recordLoc, Integer.parseInt(number));
             Record r=source.read(k);
             if (r.Phone.equals("delete")&&level== SStables.size()){
                 continue;
             }
             buffer.add(source.read(k));
-            if (buffer.size()==maxSize){
+            if (buffer.size()==maxSize||(level!=1&&SStables.get(level-2).subSet(start,end).size()>10)){
                 String filename=randomFileName();
                 String loc=fileBaseDir+"/level"+level+"/"+filename;
                 SStables.get(level-1).add(new SSTable(loc,buffer,filename));
                 level_size.set(level-1,level_size.get(level-1)+maxSize);
                 buffer=new ArrayList<>();
+                start.begin=-1;
             }
         }
         if (!buffer.isEmpty()){
@@ -343,6 +368,93 @@ public class LSMTree {
             level_size.set(level-1,level_size.get(level-1)+buffer.size());
         }
     }
+
+    public void tableInitialization(String fileBaseDir){
+
+        File table = new File(fileBaseDir);
+        if(table.list().length == 0){
+            return;
+        }
+        ArrayList<String> level0paths = getFile(fileBaseDir + "/level0");
+        level_size.add(0,0);
+        for(int i = 0; i < level0paths.size(); i++){
+            SSTable temp = new SSTable();
+            temp.loc = level0paths.get(i);
+            String[] splits = level0paths.get(i).split("/");
+            temp.id = splits[splits.length - 1];
+            level0.add(temp);
+            Integer value = level_size.get(0);
+            value = value + 1;
+            level_size.set(0, value);
+        }
+
+        for(int j = 1; j < getDir(fileBaseDir).size(); j++){
+            level_size.add(j, 0);
+            TreeSet<SSTable> level = new TreeSet<SSTable>(new Comparator<SSTable>() {
+                @Override
+                public int compare(SSTable o1, SSTable o2) {
+                    return (o1.loc == o2.loc) ? 0 : 1;
+                }
+            });
+            ArrayList<String> levelpaths = getFile(fileBaseDir + "/level" + j);
+            for(int i = 0; i < levelpaths.size(); i++){
+                SSTable temp = new SSTable();
+                temp.loc = levelpaths.get(i);
+                String[] splits = levelpaths.get(i).split("/");
+                temp.id = splits[splits.length - 1];
+                level.add(temp);
+                Integer value = level_size.get(j);
+                value = value + 1;
+                level_size.set(j, value);
+            }
+            SStables.add(level);
+        }
+
+//TODO: level 1 - more initialization
+    }
+
+    public ArrayList<String> getFile(String path){
+        ArrayList<String> result = new ArrayList<String>();
+        File file = new File(path);
+        File[] array = file.listFiles();
+        if(array == null) return null;
+        for(int i = 0; i < array.length; i++){
+            if(array[i].isFile()){
+                result.add(array[i].getPath());
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<String> getDir(String path){
+        ArrayList<String> result = new ArrayList<String>();
+        File file = new File(path);
+        File[] array = file.listFiles();
+        for(int i = 0; i < array.length; i++){
+            if(array[i].isDirectory()){
+                result.add(array[i].getName());
+            }
+        }
+        return result;
+    }
+
+    public static String getDate(){
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String currentTime = dateFormat.format(now);
+        return currentTime;
+    }
+
+    public static void logWriter(String logContext, String logPath) throws IOException {
+        FileWriter fw = new FileWriter(logPath, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(logContext);
+        bw.newLine();
+        bw.close();
+        fw.close();
+    }
+
+
 
     class SSTable implements Comparable<SSTable>{
         String loc;
--- LSM/LSMmyPTA.java
+++ LSM/LSMmyPTA.java
@@ -1,8 +1,10 @@
 package LSM;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 
 public class LSMmyPTA {
@@ -17,49 +19,56 @@ public class LSMmyPTA {
         bufferSize=Integer.parseInt(args[1]);
         // A hashMap to store tables
         tables = new HashMap<>();
-        File root = new File("./test");
+        File root = new File("./tables");
         File[] tableList = root.listFiles();
+        //logging
+        String currentTime = getDate();
+        String logPath = "./Logging/" + "log" + currentTime;
+        String pathname = "./script.txt";
+        // A global Buffer
+        buffer = new LSMBuffer(bufferSize);
         for (int i = 0; i < tableList.length; i++){  //initialize all existing tables
             String[] temp = tableList[i].toString().split("/");
             if(temp[temp.length - 1].length() == 1){
-                tables.put(temp[temp.length - 1], new LSMTree(temp[temp.length - 1], sstableSize,"./test/" + temp[temp.length - 1], buffer));
+                tables.put(temp[temp.length - 1], new LSMTree(temp[temp.length - 1], sstableSize,"./tables/" + temp[temp.length - 1], buffer, logPath));
             }
         }
 //        tables.put("X", new Table("X"));
 //        tables.put("Y", new Table("Y"));
-        // A global Buffer
-        buffer = new LSMBuffer(bufferSize);
 
         // read script
-        readScript();
+        readScript(pathname, logPath);
         for (LSMTree lsmtree:tables.values()){
             //lsmtree.deleteTable();
         }
+
+
     }
 
-    public static void readScript() throws Exception {
-        String pathname = "./script.txt";
+    public static void readScript(String pathname, String logPath) throws Exception {
         try(FileReader reader = new FileReader(pathname);
             BufferedReader br = new BufferedReader(reader)){
             String line;
             while((line = br.readLine()) != null){
-                System.out.println(buffer.keySet());
+                logWriter("", logPath);  //empty line to separate each command in log
+                logWriter(line, logPath);
+                //System.out.println(buffer.keySet());
                 String[] keywords = operationAnalyzer(line);
                 switch (keywords[0]) {
                     case "R":
-                        read(keywords[1], Integer.parseInt(keywords[2]));
+                        read(keywords[1], Integer.parseInt(keywords[2]), logPath);
                         break;
                     case "W":
-                        write(keywords[1], keywords[2]);
+                        write(keywords[1], keywords[2], logPath);
                         break;
                     case "M":
-                        showUserWithAreaCode(keywords[1], keywords[2]);
+                        showUserWithAreaCode(keywords[1], keywords[2], logPath);
                         break;
                     case "E":
-                        erase(keywords[1], Integer.parseInt(keywords[2]));
+                        erase(keywords[1], Integer.parseInt(keywords[2]), logPath);
                         break;
                     default:
-                        delete(keywords[1]);
+                        delete(keywords[1], logPath);
                         break;
                 }
             }
@@ -78,37 +87,42 @@ public class LSMmyPTA {
     }
 
     // Retrieve the record with ID=val in table. If table does not exist, the read is aborted.
-    private static void read(String tableName, int val) throws Exception {
+    private static void read(String tableName, int val, String logPath) throws Exception {
 
         if(!tables.containsKey(tableName)){
             System.out.println("The table does not exist, the read is aborted.");
+            logWriter("The table does not exist, the read is aborted.", logPath);
         }
         else{
             LSMTree t = tables.get(tableName);
             Record r = t.readID(val);
             if(r == null){
                 System.out.println("The table " + tableName + " doesn't have record with ID = " + val);
+                logWriter("The table " + tableName + " doesn't have record with ID = " + val, logPath);
             }
             else{
                 System.out.println("Read: " + r.toString());
+                logWriter("Read: " + r.toString() + val, logPath);
             }
         }
 
     }
-    public static void erase(String tableName, int val) throws Exception{
+    public static void erase(String tableName, int val, String logPath) throws Exception{
         if(!tables.containsKey(tableName)){
             System.out.println("The table does not exist, the erase is aborted.");
+            logWriter("The table does not exist, the erase is aborted.", logPath);
         }
         else{
             LSMTree t = tables.get(tableName);
             t.deleteRecord(val);
+            logWriter("Erased: " + tableName + " " + val, logPath);
         }
     }
-    private static void write(String tableName, String recordValue) throws Exception {
+    private static void write(String tableName, String recordValue, String logPath) throws Exception {
         if(!tables.containsKey(tableName)){
-            File dir = new File("./" + tableName+"/");
-            dir.mkdirs();
-            tables.put(tableName, new LSMTree(tableName,sstableSize,"./test/"+tableName,buffer));  //TODO: pass parameters to LSMTree to create new table
+            tables.put(tableName, new LSMTree(tableName,sstableSize,"./tables/"+tableName,buffer, logPath));  //TODO: pass parameters to LSMTree to create new table
+            File thisTable = new File("./tables/"+tableName);
+            thisTable.mkdirs();
             //not add second memtable for now
             System.out.println("The table " + tableName + " does not exist, it is created.");
         }
@@ -118,7 +132,7 @@ public class LSMmyPTA {
         System.out.println("Write: " + r.toString() +" to " + tableName + " successfully!");
     }
 
-    private static void showUserWithAreaCode(String tableName, String areaCode) {
+    private static void showUserWithAreaCode(String tableName, String areaCode, String logPath) throws IOException {
         if (!tables.containsKey(tableName)) {
             System.out.println("The table does not exist, show user with area code is aborted.");
         } else {
@@ -127,22 +141,39 @@ public class LSMmyPTA {
             System.out.println(matchedRecords);
             if (matchedRecords.size() == 0) {
                 System.out.println("The table " + tableName + " doesn't have record with area code = " + areaCode);
+                logWriter("The table " + tableName + " doesn't have record with area code = " + areaCode, logPath);
             } else {
                 for (Record matchedRecord : matchedRecords) {
                     System.out.println("MRead: " + matchedRecord.toString());
+
                 }
             }
         }
     }
 
-    private static void delete(String tableName){
+    private static void delete(String tableName, String logPath) throws IOException {
         LSMTree t=tables.get(tableName);
         t.deleteTable();
         tables.remove(tableName);
         System.out.println("Table " + tableName + " has been dropped!");
+        logWriter("Deleted: " + tableName, logPath);
     }
 
+    public static String getDate(){
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String currentTime = dateFormat.format(now);
+        return currentTime;
+    }
 
+    public static void logWriter(String logContext, String logPath) throws IOException {
+        FileWriter fw = new FileWriter(logPath, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(logContext);
+        bw.newLine();
+        bw.close();
+        fw.close();
+    }
 
 
 }
